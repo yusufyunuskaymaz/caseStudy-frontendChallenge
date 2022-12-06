eee5fbc50c98bb675cb8088fa691db816f6e2efc
yusufKaymaz <kaymazyusuf88@gmail.com>
2022-12-06 13:13:55 +0300

    movies

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import PlaceHolder from  "../img/placeholder.png"
 
 const Home = () => {
   return (
     <main>
-        <div className="container">
-        <div className="box"><h1>Series</h1></div>
-        <div className="box"><h1>Movies</h1></div>
+        <div className="container d-flex py-5">
+        <div type="button" className="box me-3" style={{backgroundImage:`url(${PlaceHolder})`, backgroundColor:"black"}}><h1 className='display-2'>Series</h1></div>
+        <div type="button" className="box" style={{backgroundImage:`url(${PlaceHolder})`, backgroundColor:"black"}}><h1 className='display-2'>Movies</h1></div>
         </div>
+        
     </main>
   )
 }
